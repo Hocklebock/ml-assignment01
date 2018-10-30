10f7d26ef69ca59b7a0e7a29653a25d02bd95e2c
Jan Fröhlich <froehlich.janp@gmail.com>
2018-10-30 11:02:20 +0100

    fixed linear regression

--- Exercise4.py
+++ Exercise4.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from random import randint
 from numpy import array, unique, mean, amax, amin, concatenate, ones, sum, dot
 
 #open file and split header from data
@@ -45,21 +46,38 @@ w = -1 * ones((2, 1))
 eta = 1e-4
 i = 0
 error = sum([(yi - dot([1, xi], w))**2 for xi, yi in zip(x, y)])
+diffold = 0
 
-while error > 1e-3 and i < 1000:
-    error = 0
-    # iterate over all data values
-    for index in range(x.shape[0]):
-        xi = array([[1, x[index]]]).T #x vector of current iteration of shape [1 \n xi]
-        diff = y[index] - dot(w.T, xi) #difference between data value an linear regression value
-        dw = eta * diff * xi # delta weights for convergence
-        w += dw
-        error += diff**2 # sum of squared errors for convergence check
+w0 = []
+w1 = []
+err = []
+
+while error > 1e-5 and i < 1000:
+    index = randint(0, x.shape[0]-1)
+    xi = array([[1, x[index]]]).T  # x vector of current iteration of shape [1 \n xi]
+    diff = y[index] - dot(w.T, xi)  # difference between data value an linear regression value
+    dw = eta * diff * xi  # delta weights for convergence
+    w += dw
+    error = (diff - diffold) ** 2  # sum of squared errors for convergence check
+    diffold = diff
     i += 1
 
-print(i, sum([(yi - dot([1, xi], w))**2 for xi, yi in zip(x, y)]))
+    #convergence parameter study
+    w0.append(w[0].flatten())
+    w1.append(w[1].flatten())
+    err.append(error.flatten())
+
+
+print(i, error)
 
 plt.plot(x, w[0]+w[1]*x)
 plt.savefig("test4c.png")
+
+f3 = plt.figure(3)
+plt.plot(range(i), err)
+f4 = plt.figure(4)
+plt.plot(range(i), w0)
+plt.plot(range(i), w1)
+
 plt.show()
 
